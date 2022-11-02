af05e8bcf2d6afd6f0cfaa5bc73dc977b8419888
PedroVitorhj <pvherrera82@gmail.com>
2022-11-02 18:10:50 -0300

    telas

---
+++ java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.tela {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.tela to javafx.fxml;
+    exports com.mycompany.tela;
+}
+*/
